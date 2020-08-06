819a94503c2579b409e1d37119a41d9a5ae918f8
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2020-08-05 22:07:51 -0300

    First commit

--- babel.config.js
+++ babel.config.js
@@ -14,9 +14,7 @@ module.exports = {
     ['module-resolver', {
       alias: {
         '@config': './src/config',
-        '@models': './src/models',
-        '@controllers': './src/controllers',
-        '@views': './src/views'
+        '@models': './src/models'
       }
     }]
   ],
--- src/controllers/UserController.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '@models/User'
-
-export class UserController {
-  show () {
-    const user = new User()
-    console.log(user)
-  }
-}
